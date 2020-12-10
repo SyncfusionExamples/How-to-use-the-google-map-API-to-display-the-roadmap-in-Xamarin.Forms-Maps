383754453c72f529c7de41dadb820ef01f2b4a79
Hemalatha <hemalathavinai@gmail.com>
2020-12-10 15:33:23 +0530

    sample added

---
+++ SfMaps_DifferentView/SfMaps_DifferentView/SfMaps_DifferentView.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SfMaps_DifferentView.Droid
+{
+    [Activity(Label = "SfMaps_DifferentView", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ SfMaps_DifferentView/SfMaps_DifferentView/SfMaps_DifferentView/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfMaps_DifferentView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();           
+        }
+
+        /// <summary>
+        /// This event is used to pass the uri of desired maps tile provider
+        /// </summary>
+        /// <param name="sender">The imagery layer</param>
+        /// <param name="e">The Tile uri arguments</param>
+        private void ImageryLayer_RequestTileUri(object sender, Syncfusion.SfMaps.XForms.TileUriArgs e)
+        {
+            var link = "http://mt1.google.com/vt/lyrs=m&x=" + e.X.ToString() + "&y=" + e.Y.ToString() + "&z=" + e.ZoomLevel.ToString();
+            e.Uri = link;
+        }
+    }
+}
